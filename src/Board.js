bd11f06f3eb88dc93d7847d9914ca7950f1b9701
HallowedDust5 <77157773+HallowedDust5@users.noreply.github.com>
2021-08-26 19:30:42 -0400

    Redid id system

--- src/Board.js
+++ src/Board.js
@@ -15,10 +15,7 @@ export class GameOfLifeBoard extends React.Component {
             winner = 
             this.props.ctx.gameover.winner !== undefined ? (<div id="winner">Winner:{this.props.ctx.gameover.winner}</div>) : (<div id="winner">Draw!</div>);
         }
-        //TODO:
-        // 1. Properly display the board
-        // 2. Make tile color and border variable based on element in array
-        // Ex: [null,P1,P2] == {null:black, P1:red, P2: blue}
+        
         const GAMEBOARD_ARR_HEIGHT = this.props.G.cells.length;
         const GAMEBOARD_ARR_LENGTH = this.props.G.cells[0].length;
         const SQUARE_DIMENSION = 300/GAMEBOARD_ARR_HEIGHT;
@@ -30,21 +27,31 @@ export class GameOfLifeBoard extends React.Component {
             textAlign: 'center',
         };
         const blankCellStyle = Object.assign({},cellStyle,{border:'0px'});
-        const redCellStyle = Object.assign({},cellStyle,{border:'1px solid red'});
-        const blueCellStyle = Object.assign({},cellStyle,{border:'1px solid blue'});
+        //Red cell styles
+        const borderRedCellStyle = Object.assign({},cellStyle,{border:'1px solid red'});
+        const filledRedCellStyle = Object.assign({},cellStyle,{background:'red'})
+
+        //Blue cell styles
+        const borderBlueCellStyle = Object.assign({},cellStyle,{border:'1px solid blue'});
+        const filledBlueCellStyle = Object.assign({},cellStyle,{background:'blue'})
 
 
+        //TODO:
+        // Redo for loop to be more readable
+        // Redo cell id system
+        // Put in conditional coloring
         let tbody =[];
         
         for(let i = 0;i < GAMEBOARD_ARR_HEIGHT; i++){
             let cells =[];
             for (let j = 0; j < GAMEBOARD_ARR_LENGTH; j++) {
-                const id = 3 * i + j;
+                //ID is where the tens digit 
+                const id = `${i},${j}`;
                 //Calculates the indices of the middle 3 elements
                 const middlethree = [0,1,2].map((x)=>{
                     return floor(GAMEBOARD_ARR_LENGTH/2)+x-1;
                 });
-                console.info(middlethree);
+
                 
                 
                 //First line JSX push
@@ -52,7 +59,7 @@ export class GameOfLifeBoard extends React.Component {
                     //Keeps the middle 3 visible
                     if(middlethree.includes(j)){
                         cells.push(
-                            <td style={redCellStyle} key={id} onClick={() => this.onClick(id)}>
+                            <td style={borderRedCellStyle} key={id} onClick={() => this.onClick(id)}>
                                 {this.props.G.cells[id]}
                             </td>
                             );
@@ -71,7 +78,7 @@ export class GameOfLifeBoard extends React.Component {
                     //Keeps the middle 3 visible
                     if(middlethree.includes(j)){
                         cells.push(
-                            <td style={blueCellStyle} key={id} onClick={() => this.onClick(id)}>
+                            <td style={borderBlueCellStyle} key={id} onClick={() => this.onClick(id)}>
                                 {this.props.G.cells[id]}
                             </td>
                             );
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,7 @@
+import { INVALID_MOVE } from "boardgame.io/core";
+
+
+
 const GAMEBOARD_HEIGHT = 12;
 const GAMEBOARD_WIDTH = 19;
 
@@ -9,7 +13,15 @@ export const GameOfLife = {
     },
 
     moves: {
-
+        clickCell:(G,ctx,id)=>{
+            let [i,j] =id.split(',').map(x=>{return parseInt(x);});
+            console.log(i,j)
+            if(G.cells[i][j] !== null) {
+                
+                return INVALID_MOVE;
+            }
+            G.cells[i][j] = ctx.currentPlayer;
+        }
     },
 
     
